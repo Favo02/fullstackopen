a13c87ee30354a8b2b9cc1974f694f5e2ce46f15
Luca Favini <luca.favini.02@gmail.com>
2022-03-27 05:45:06 +0200

    2.4, 2.5: Part 2 - Exercises 4, 5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,44 +1,52 @@
-import Course from "./components/Course.js"
+import Courses from "./components/Courses.js"
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a puffo',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'State of a cane',
-        exercises: 13,
-        id: 4
-      },
-      {
-        name: 'State of a gatto',
-        exercises: 11,
-        id: 5
-      },
-      {
-        name: 'State of a component',
-        exercises: 5,
-        id: 6
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return <Courses courses={courses} />
 }
 
 export default App
