2973f92aa033ac3c7285a9b47db6ae15967c39a5
Luca Favini <luca.favini.02@gmail.com>
2022-03-27 07:29:03 +0200

    2.9: Part 2 - Exercise 9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,39 +1,53 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1233333' }
-  ]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const addPerson = (event) => {
-    event.preventDefault()
-    const newPerson = {}
-    newPerson.name = newName
-    newPerson.number = newNumber
+    event.preventDefault();
+    const newPerson = {};
+    newPerson.name = newName;
+    newPerson.number = newNumber;
 
-    if (persons.filter( person => person.name === newPerson.name).length > 0) {
-      alert(`${newName} is already added to phonebook`)
-      return
+    if (persons.filter((person) => person.name === newPerson.name).length > 0) {
+      alert(`${newName} is already added to phonebook`);
+      return;
     }
 
-    let copy = [...persons]
-    copy = copy.concat(newPerson)
-    setPersons(copy)
-  } 
+    let copy = [...persons];
+    copy = copy.concat(newPerson);
+    setPersons(copy);
+  };
 
   const editInputName = (event) => {
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const editInputNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
+    setNewNumber(event.target.value);
+  };
+
+  const editFilter = (event) => {
+    setFilter(event.target.value);
+  };
+
+  const filtered = persons.filter((person) =>
+    person.name.toLowerCase().includes(filter)
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      Filter shown with <input onChange={editFilter} />
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input onChange={editInputName} />
@@ -45,9 +59,13 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      { persons.map( person => ( <p key={person.name}>{person.name} {person.number}</p> )) }
+      {filtered.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
