98349ce6a4aa0f9281f480bcfbdade69c0abd8ab
Luca Favini <luca.favini.02@gmail.com>
2022-03-27 06:56:23 +0200

    2.6: Part 2 - Exercise 6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const newPerson = {}
+    newPerson.name = newName
+    let copy = [...persons]
+    copy = copy.concat(newPerson)
+    setPersons(copy)
+  } 
+
+  const editInput = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input onChange={editInput} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      { persons.map( person => ( <p key={person.name}> {person.name} </p> )) }
+    </div>
+  )
+}
+
+export default App
