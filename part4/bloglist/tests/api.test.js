264777676eb25151105eba0c33e41913e38cf815
Luca Favini <luca.favini.02@gmail.com>
2022-04-07 12:29:09 +0200

    4.10: Part 4 - Exercise 10

--- part4/bloglist/tests/api.test.js
+++ part4/bloglist/tests/api.test.js
@@ -31,6 +31,29 @@ test('id is defined', async () => {
     expect(ids).not.toContain(undefined)
 })
 
+test('a valid blog can be added ', async () => {
+    const newBlog = {
+        title: "The smurfs",
+        author: "Papa smurf",
+        url: "https://smurfs.com/",
+        likes: 15,
+    }
+  
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const contents = blogsAtEnd.map(n => n.title)
+    expect(contents).toContain(
+        'The smurfs'
+    )
+})
+
 afterAll(() => {
     mongoose.connection.close()
 }) 
