eae29e5ad4109cd3176478d1aac0b5c75f4c158b
Luca Favini <luca.favini.02@gmail.com>
2022-04-11 15:20:31 +0200

    5.7: Part 5 - Exercise 7

--- part5/bloglist/src/components/Blog.js
+++ part5/bloglist/src/components/Blog.js
@@ -1,7 +1,32 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+    const [viewDetails, setViewDetails] = useState(false)
+
+    const blogStyle = {
+        'border': '1px solid black',
+        'borderRadius': '5px',
+        'marginBottom': '5px',
+        'padding': '5px 10px',
+        'maxWidth': '400px'
+    }
+
+    if (viewDetails) {
+        return (
+            <div style={blogStyle}>
+                {blog.title} <button onClick={() => setViewDetails(false) }>hide</button><br />
+                {blog.url}<br />
+                {blog.likes} <button>like</button><br />
+                {blog.url}<br />
+            </div>
+        )
+    }
+
+    return (
+        <div style={blogStyle}>
+            {blog.title} {blog.author} <button onClick={() => setViewDetails(true) }>view</button>
+        </div>
+    )
+}
 
 export default Blog
