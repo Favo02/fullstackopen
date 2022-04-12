80bc890c661e6e14245a367af84fa1a34e6ed3d5
Luca Favini <luca.favini.02@gmail.com>
2022-04-12 18:48:56 +0200

    5.18: Part 5 - Exercise 18

--- part5/bloglist/cypress/integration/bloglist.spec.js
+++ part5/bloglist/cypress/integration/bloglist.spec.js
@@ -10,7 +10,28 @@ describe("login checks", function() {
         cy.visit("http://localhost:3000")
     })
 
-    it("login form at start", function() {
+    it("Login form is shown", function() {
         cy.contains("Log in to application")
+    })
+
+    describe("Login",function() {
+        it("succeeds with correct credentials", function() {
+            cy.get("#username-input").type("test")
+            cy.get("#password-input").type("test")
+            cy.get("#login-button").click()
+
+            cy.contains("test logged in")
+        })
+
+        it("fails with wrong credentials", function() {
+            cy.get("#username-input").type("ehehe")
+            cy.get("#password-input").type("wrong")
+            cy.get("#login-button").click()
+
+            cy.contains("wrong credentials")
+                .should("have.css", "color", "rgb(255, 0, 0)")
+
+            cy.get("html").should("not.contain", "test logged in")
+        })
     })
 })
